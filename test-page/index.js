eb33e00262fbb4382ba3153bcf215702170d117c
samussiah <samussiah@gmail.com>
2019-04-22 10:44:37 -0400

    plot month, week, and day

--- test-page/index.js
+++ test-page/index.js
@@ -1,18 +1,20 @@
-fetch('../../data/10295/day.csv')
+const width = Math.floor(d3.select('#container').node().offsetWidth/3) - 3;
+fetch('../../data/10295/month.csv')
     .then(response => response.text())
     .then(text => {
         const ts = timeSeries(
             d3.csvParse(text),
             {
                 x: {
-                    "field": "date",
-                    "format": "%Y-%m-%d"
+                    "field": "month",
+                    "format": "%Y-%m"
                 },
                 y: {
                     "field": "mean"
                 },
+                width
             },
-            '#container',
+            '.container--month .chart',
         );
     });
 fetch('../../data/10295/week.csv')
@@ -28,24 +30,26 @@ fetch('../../data/10295/week.csv')
                 y: {
                     "field": "mean"
                 },
+                width
             },
-            '#container',
+            '.container--week .chart',
         );
     });
-fetch('../../data/10295/month.csv')
+fetch('../../data/10295/day.csv')
     .then(response => response.text())
     .then(text => {
         const ts = timeSeries(
             d3.csvParse(text),
             {
                 x: {
-                    "field": "month",
-                    "format": "%Y-%m"
+                    "field": "date",
+                    "format": "%Y-%m-%d"
                 },
                 y: {
                     "field": "mean"
                 },
+                width
             },
-            '#container',
+            '.container--day .chart',
         );
     });
